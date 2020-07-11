b68675f55018d3dba366c5a2e3b78d753269829d
dwkns <dwkns@me.com>
2020-07-11 19:10:02 +0100

    loads of homepage updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+document.addEventListener("DOMContentLoaded", function () {
+  var menuButton = document.getElementById("menuBtn");
+  menuButton.addEventListener("click", navToggle);
+
+  function navToggle() {
+    var menu = document.getElementById("menu");
+    var burger = document.getElementById("nav-burger-icon");
+    var close = document.getElementById("nav-close-icon");
+    burger.classList.toggle("hidden");
+    close.classList.toggle("hidden");
+    menu.classList.toggle("hidden");
+  }
+});
