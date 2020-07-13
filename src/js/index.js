e54c1e06795519734310b69375be1a45b410d314
dwkns <dwkns@me.com>
2020-07-13 20:43:54 +0100

    Moved id's out of SVG's as htmlnano strips them on build.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  console.log('DOM content loaded')
   var menuButton = document.getElementById("menuBtn");
+
+
   menuButton.addEventListener("click", navToggle);
 
   function navToggle() {
     var menu = document.getElementById("menu");
+    console.log('navToggle clicked')
     var burger = document.getElementById("nav-burger-icon");
+    console.log(burger)
+
     var close = document.getElementById("nav-close-icon");
+    console.log(close)
+
+
+    
     burger.classList.toggle("hidden");
     close.classList.toggle("hidden");
     menu.classList.toggle("hidden");
